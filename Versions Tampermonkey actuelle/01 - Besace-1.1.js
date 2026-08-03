4eafd2400227700303215577c032734b5c0d0a7d
albertleblaireau <albert.le.blaireau@gmail.com>
2026-08-03 21:26:34 +0200

    tes refresh

--- Versions Tampermonkey actuelle/01 - Besace-1.1.js
+++ Versions Tampermonkey actuelle/01 - Besace-1.1.js
@@ -198,7 +198,7 @@
 
     fenetre.innerHTML = `
         <h3 style="margin-top:0;text-align:center;">
-            🎒 BESACE DU RÔDEUR M
+            🎒 BESACE DU RÔDEUR Max
         </h3>
 
         <div style="margin-bottom:8px;">
